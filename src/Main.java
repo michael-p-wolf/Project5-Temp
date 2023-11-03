68f2fd64df3f3f6a58ddda5279dff534fa68ccb3
michael-p-wolf <wolf180@purdue.edu>
2023-11-03 16:56:50 -0400

    Editing main method

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,54 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello World!");
+        Scanner scan = new Scanner(System.in);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int input = 0;
+        String email = "";
+        String password = "";
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        do {
+            System.out.println("Hello!\n[1] Sign In\n[2] Create Account\n[3] Exit");
+            input = scan.nextInt();
+            scan.nextLine();
+            if (input == 1) {
+                System.out.println("Email:");
+                email = scan.nextLine();
+                System.out.println("Password:");
+                password = scan.nextLine();
+            } else if (input == 2) {
+                System.out.println("[1] Create Seller Account\n[2] Create Buyer Account\n[3] Back");
+                input = scan.nextInt();
+                scan.nextLine();
+                if (input == 1) {
+                    do {
+                        System.out.println("Email:");
+                        email = scan.nextLine();
+                        System.out.println("Password:");
+                        password = scan.nextLine();
+                        System.out.println("Confirm Password:");
+                        if (!scan.nextLine().equals(password)) {
+                            System.out.println("Error! PasswordS must match!");
+                        }
+                    } while ()
+
+                } else if (input == 2) {
+
+                } else if (input == 3) {
+
+                } else {
+                    input = 0;
+                }
+
+            } else if (input == 3){
+                System.out.println("Goodbye!");
+                break;
+            } else {
+                System.out.println("Error! Invalid input!");
+                input = 0;
+            }
+        } while (input == 0);
+
     }
 }
