3c8430493ede2c2bd4c4280ecdd590b6d8c0dcb4
simratkaurthind <thind0@purdue.edu>
2023-11-03 16:14:50 -0400

    product getters and setters

--- src/Product.java
+++ src/Product.java
@@ -13,6 +13,46 @@ public class Product {
         this.filename = filename;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
     public String toString() {
         return String.format("%s,%s,%d,%f", this.name, this.description, this.price,this.quantity);
     }
