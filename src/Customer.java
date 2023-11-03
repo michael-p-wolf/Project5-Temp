99fd757bdfc27b1f87c41843dce2efb407bcf35d
Pranay Nandkeolyar <pranay.nandkeolyar@gmail.com>
2023-11-03 17:05:27 -0400

    Create Customer.java

---
+++ src/Customer.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer extends Person {
+    private int customerID;
+    private String username;
+    private String password;
+    private String storeName;
+    private ArrayList<Product> cart;
+    private ArrayList<Product> purchaseHistory;
+
+    private ArrayList<Store> stores;
+
+
+
+}
